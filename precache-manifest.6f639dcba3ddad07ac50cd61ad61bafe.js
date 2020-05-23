41261d060160944539ac8c63c12c6aa53e16ab66
hurri388 <hurri388@outlook.com>
2020-05-23 21:36:42 +0500

    Updates

---
+++ precache-manifest.6f639dcba3ddad07ac50cd61ad61bafe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3aa08c9b4ed8a49e8ba2b09fadb109c",
+    "url": "/Celebrity-Detector/index.html"
+  },
+  {
+    "revision": "a325b3f26ccc2cb0ae12",
+    "url": "/Celebrity-Detector/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "f21a94f46424475fbdf0",
+    "url": "/Celebrity-Detector/static/css/main.0b4a9701.chunk.css"
+  },
+  {
+    "revision": "a325b3f26ccc2cb0ae12",
+    "url": "/Celebrity-Detector/static/js/2.303fa654.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/Celebrity-Detector/static/js/2.303fa654.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f21a94f46424475fbdf0",
+    "url": "/Celebrity-Detector/static/js/main.880c76eb.chunk.js"
+  },
+  {
+    "revision": "f4ce8e13a46074f69382",
+    "url": "/Celebrity-Detector/static/js/runtime-main.e4a5808e.js"
+  },
+  {
+    "revision": "cefc20232703e5e3c24efd5f50d75e26",
+    "url": "/Celebrity-Detector/static/media/GitHub_Logo.cefc2023.png"
+  }
+]);
